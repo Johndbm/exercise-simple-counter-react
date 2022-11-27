707a191fb63bcac365098015713d4ba905578ad5
john.banks <johndbanksm@gmail.com>
2022-11-27 21:53:17 +0000

    code updates, counter logic

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Counter = () => {
+	return (
+		<>
+			Logic
+		</>
+	);
+};
+
+
+
+
+export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,14 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import "font-awesome/css/font-awesome.min.css"
 
 //import your own components
 import Home from "./component/home.jsx";
 
 //render your react application
+const counter = 0;
+
+
+
 ReactDOM.render(<Home />, document.querySelector("#app"));
